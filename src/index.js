575e174391eaa753b8c59fbaf0b29cc6283eda76
Julia Bazaeva <truckfulofcandy@gmail.com>
2021-08-17 21:52:51 +0700

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+ let reversed = 0;
+ let digit = 0;
+ let num  = Math.abs(n);
+   
+ while(num) {
+  digit = num % 10;
+  reversed = reversed * 10 + digit;
+  num = Math.floor(num / 10);  
+ }
+ return reversed;
 }
